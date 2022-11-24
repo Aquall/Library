8064788af5a3d4ad4d94e75fe8f9a893d065bcc2
Nikita <nikitka_bodr@mail.ru>
2022-11-25 00:19:10 +0300

    L-008/add author migration

--- db/migrate/20221123153657_create_authors.rb
+++ db/migrate/20221123153657_create_authors.rb
@@ -1,8 +1,8 @@
 class CreateAuthors < ActiveRecord::Migration[7.0]
   def change
     create_table :authors do |t|
-      t.string :name
-
+      t.references :people, null: false, foreign_key: true
+      
       t.timestamps
     end
   end
