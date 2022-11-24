76e952fd811d8854c1af49f689a6670b3a3dfabd
Nikita <nikitka_bodr@mail.ru>
2022-11-24 23:07:08 +0300

    L-007/add schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,3 +9,15 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_11_23_174237) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+end
