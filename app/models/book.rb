164b7c7b825216788de32d565082594a96bd60f2
Nikita <nikitka_bodr@mail.ru>
2022-11-24 22:56:36 +0300

    L-007/fix book validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
   validates :name, presence: {message: 'must be abided!'}
-  validates :year, comparison: {less_than_or_equal_to: DateTime.now.to_date.year}
+  validates :year, numericality: {less_than_or_equal_to: DateTime.now.to_date.year}
   validates :name, uniqueness:{scope: :year, message: 'must be unique!'}
 end
