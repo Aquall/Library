a752c0bc07ea57213d9f541dfec1d3c35e5943ad
Nikita <nikitka_bodr@mail.ru>
2022-11-24 20:58:46 +0300

    L-007/fix book migration

--- db/migrate/20221122223347_create_books.rb
+++ db/migrate/20221122223347_create_books.rb
@@ -2,7 +2,7 @@ class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
       t.string :name
-      t.date :year
+      t.integer :year
 
       t.timestamps
     end
